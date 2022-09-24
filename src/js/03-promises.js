8ca3141c06c5a36b570dd272cf6870f26bed99c9
unknown <kononov8214@gmail.com>
2022-09-24 19:47:27 +0300

    promises updated

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,4 @@
 import Notiflix from 'notiflix';
-import { Report } from 'notiflix/build/notiflix-report-aio';
 
 const refs = {
   form: document.querySelector('form'),
@@ -21,26 +20,16 @@ function createPromise(position, delay) {
 }
 
 function getInputValue(currentTarget) {
-  // let userDelay = Number(currentTarget.delay.value);
-  // let userStep = Number(currentTarget.step.value);
-  // let userAmount = Number(currentTarget.amount.value);
   return {  userDelay:Number(currentTarget.delay.value),
             userStep:Number(currentTarget.step.value),
             userAmount: Number(currentTarget.amount.value)
   }; 
-  // console.log('{ userDelay, userStep, userAmount }: ', { userDelay, userStep, userAmount });
-  // console.log('currentTarget.elements: ', currentTarget.elements);
 }
 
   refs.form.addEventListener('submit', (event) => {
     event.preventDefault();
-    // const inputs =  { userDelay: Number(event.currentTarget.delay.value),
-    //                   userStep: Number(event.currentTarget.step.value),
-    //                   userAmount: Number(event.currentTarget.amount.value)
-    //                 } 
+
     let {userDelay, userStep, userAmount } = getInputValue(event.currentTarget);
-    // console.log('getInputValue', getInputValue(event.currentTarget));
-    console.log('userAmount: ', userAmount);
     
     for (let position = 1; position <= userAmount; position += 1) {
       createPromise(position, userDelay).then(({ position, delay }) => {
